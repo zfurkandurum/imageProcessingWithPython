378f40f7f977973da9cc15dc4f1a88a5b6da27ad
zfdurum <zfurkandurum@gmail.com>
2023-08-11 21:25:27 +0300

    mouse

--- example/mouseEvent.py
+++ example/mouseEvent.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+####1
+"""
 def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x,', ', y)
@@ -16,10 +18,34 @@ def click_event(event, x, y, flags, params):
         strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
         cv2.putText(img, strBGR, (x, y), font, .5, (0,255,255), 2)
         cv2.imshow('image', img)
+"""
+#####2
+"""
+def click_event(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 3, (0,0,255), -1)
+        points.append((x,y))
+        if len(points) >=2:
+            cv2.line(img, points[-1], points[-2], (255,0,0), 5)
+        cv2.imshow('image', img)
+"""
+####3
+def click_event(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        blue = img[x, y, 0]
+        green= img[x, y, 1]
+        red = img[x, y, 2]
+        cv2.circle(img, (x,y), 3, (0,0,255), -1)
+        myColorImage = np.zeros((512,512,3), np.uint8)
 
+        myColorImage[:] = [blue, green, red]
+        cv2.imshow('color', myColorImage)        
+   
 
+#img = np.zeros((512,512,3), np.uint8)
 img = cv2.imread("data/aloeR.jpg")
 cv2.imshow('image', img)
+points = []
 
 cv2.setMouseCallback("image", click_event)
 
