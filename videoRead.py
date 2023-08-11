eccd362a58339d895e0188217982c852cae4228f
zfdurum <zfurkandurum@gmail.com>
2023-08-11 09:45:18 +0300

    video

--- videoRead.py
+++ videoRead.py
@@ -6,8 +6,8 @@ cap = cv2.VideoCapture(0)
 while(cap.isOpened()):
     ret, frame = cap.read()
     
-    cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('video', gray)
